0bfb0977287505b8f8e4cc3f80b9a10da654dfdf
Nirob Ahmed <neeerob.ahmed@gmail.com>
2022-09-22 21:22:04 +0600

    LabTask1[CV] is complete

---
+++ LabTask-1[CV]/myCV/myCV/Controllers/myCVController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myCV.Controllers
+{
+    public class myCVController : Controller
+    {
+        // GET: myCV
+        public ActionResult PersonalInfo()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Project()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ LabTask-1[CV]/myCV/myCV/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="myCV.MvcApplication" Language="C#" %>
